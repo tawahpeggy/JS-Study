4fa2c9617210ca1c10e2a0be3327b1b30ab85e7a
tawahpeggy <tawahpeggy98@gmail.com>
2022-03-12 02:15:05 +0100

    variables and primitive datatypes

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log('Hello World');
+// variables and data types
+var firstName = "peggy";
+var lastName = "tawah",
+age = 24,
+level= 11
+;
+var person='teacher';
+console.log(person);
+person = 'Engineer'
+console.log(firstName,lastName,level,age,person);
+// primitive data types
+
+// string
+var str = "john is an instructor";
+console.log(str);
+
+// number
+var num =27;
+console.log(num);
+
+// boolean
+var num1=45;
+var num2 = 50;
+if(num1!==num2) {
+    console.log(false);
+}
+
+// undefined
+var a;
+console.log(a);
+
+// b is not defined
+// console.log(b);
+
+// null
+var c = null;
+console.log(c);
+
+
